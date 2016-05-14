7a73cbb805536bbfe93ea371d271aa8c7b7edcd9
Matthew Nerger <mnerger@lsu.edu>
2016-05-14 13:41:36 -0500

    Test menu opening function

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -46,11 +46,12 @@ $(function() {
           expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        it('changes visibility on click', function() {
+          $('.menu-icon-link').click();
+          expect($('body').hasClass('menu-hidden')).toBe(false);
+          $('.menu-icon-link').click();
+          expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
     });
 
